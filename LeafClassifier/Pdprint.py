96f2c214eee470807706ff0c3b123383ba09558f
dangspin <dangxiaoqian@gmail.com>
2018-03-02 19:35:54 +0800

    Add pearson number

--- LeafClassifier/Pdprint.py
+++ LeafClassifier/Pdprint.py
@@ -8,7 +8,7 @@ Author: Charlie Dang
 Date: March/01/2018
 
 """
-
+import numpy as np
 import pandas as pd
 
 def get_info(Dataframe):
@@ -58,3 +58,32 @@ def list_null(Dataframe):
     print (Dataframe.shape[1],' columns: ')
     for i in Dataframe.columns.values:
         print (i, ": nan", Dataframe[i].isnull().sum(), ', ', Dataframe[i].dtypes)
+
+
+def Pearson(X, Y):
+    """
+    This function calculate the Pearson correlation function. Here we use the unbiased version.
+    The description of the coefficient could be refered here: https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
+
+    Parameters:
+    -----------
+    X: A numpy array, which has the dimension the same as the number of observations
+    Y: A numpy array, which has the dimension the same as the number of observations
+    len(X) == len(Y)
+
+    Returns:
+    --------
+    float number, unbiased Pearson correlation number
+
+    """
+    if (len(X) != len(Y)):
+        raise Exception("Cannot calculate the coefficient in this case. Please make sure len(X) == len(Y)!")
+
+    ## convert list or np.array to numpy array
+    X = np.array(X)
+    Y = np.array(Y)
+
+    ## Equation: (E(XY)-E(X)E(Y))/(Std(X)Std(Y))
+    sumxy = np.dot(X, Y)
+
+    return (sumxy - len(X)*np.mean(X)*np.mean(Y))/((len(X)-1)*np.std(X, ddof=1)*np.std(Y,ddof=1))
